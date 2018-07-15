01ab2bc553c0e6cef1f83a01295e7bd786d957bd
Traci <traci.kamp@blackburn.edu>
2018-07-15 17:32:51 -0500

    Add New Feed Selection suite

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -77,12 +77,13 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+    });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
-
+    describe('New Feed Selection', function() {
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
-    }); 
+    });
 }());
